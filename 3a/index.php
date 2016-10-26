e8c50b130f5b74331f2a16bcbe162809a9b46a5a
Jay Sprouse <jaysprouse2689@gmail.com>
2016-10-26 18:58:40 -0500

    added php

--- 3a/index.php
+++ 3a/index.php
@@ -1,17 +1,12 @@
-Insert two numbers in the next form and hit submit button 
-<form action="calculation.php" method="post"> 
-	Firstnumber: <input name="num1" type="text" />
-	Secondnumber: <input name="num2" type="text" /> 
-	<input type="submit" />
-</form>
-
-
 <?php
-	$num1 = $_POST['num1'];
-	$num2 = $_POST['num2'];
-	$a = $num1 + $num2;
-	$b = $num1 - $num2; {
-	echo "The sum of the two numbers is ". $a;}
-	{
-	echo "The difference of the two numbers is ". $b;}
+	function() {
+		$num1 = $_GET['num1'];
+		$num2 = $_GET['num2'];
+		$addition = $_GET['operator'];
+		if ($addtion == "+") {
+			echo $num1 + $num2;
+		}
+	}
 ?>
+
+<?php function () ?>
