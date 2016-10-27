f9e57140613a21ab5e44fddc79b391ecad25288a
Nikki <nicleemart@gmail.com>
2016-10-26 19:17:06 -0500

    addition works

--- 3a/index.php
+++ 3a/index.php
@@ -2,9 +2,10 @@
 	function calculator() {
 		$num1 = $_GET['num1'];
 		$num2 = $_GET['num2'];
-		$addition = $_GET['operator'];
-		if ($addition == '+') {
-			echo '$num1 + $num2';
+		$operator = $_GET['operator'];
+
+		if ($operator == 'add') {
+			echo $num1 + $num2;
 		}
 	}
 ?>
