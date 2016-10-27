13b52d1af4de504b07e92df5fcaf36b4a45a0086
Jay Sprouse <jaysprouse2689@gmail.com>
2016-10-26 19:10:12 -0500

    wip

--- 3a/index.php
+++ 3a/index.php
@@ -1,12 +1,12 @@
 <?php
-	function() {
+	function calculator() {
 		$num1 = $_GET['num1'];
 		$num2 = $_GET['num2'];
 		$addition = $_GET['operator'];
-		if ($addtion == "+") {
-			echo $num1 + $num2;
+		if ($addition == '+') {
+			echo '$num1 + $num2';
 		}
 	}
 ?>
 
-<?php function () ?>
+<?php echo calculator(); ?>
